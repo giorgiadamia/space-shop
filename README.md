a4d4fefdc66f389995b182190d88cd9a3e4195d9
Giorgi <1giorgiadamia@gmail.com>
2023-01-02 18:05:53 +0400

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Shop
+
+Learning microservices with Spring Framework and it's friends.
---
+++ product-service/src/test/java/com/karavan/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karavan.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
