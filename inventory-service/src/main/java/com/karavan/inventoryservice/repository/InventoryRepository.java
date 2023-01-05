873393cdca4cc83dc13256d9687dbf15d3b46ec1
Giorgi <1giorgiadamia@gmail.com>
2023-01-06 00:13:14 +0400

    implemented inventory service and made configuration for inter service communication

---
+++ inventory-service/src/main/java/com/karavan/inventoryservice/repository/InventoryRepository.java
@@ -0,0 +1,10 @@
+package com.karavan.inventoryservice.repository;
+
+import com.karavan.inventoryservice.model.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+    Optional<Inventory> findBySkuCode(String skuCode);
+}
---
+++ inventory-service/src/main/java/com/karavan/inventoryservice/service/InventoryService.java
@@ -0,0 +1,18 @@
+package com.karavan.inventoryservice.service;
+
+import com.karavan.inventoryservice.repository.InventoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class InventoryService {
+
+    private final InventoryRepository inventoryRepository;
+
+    @Transactional(readOnly = true)
+    public boolean isInStock(String skuCode) {
+        return inventoryRepository.findBySkuCode(skuCode).isPresent();
+    }
+}
